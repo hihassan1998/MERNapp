f461026a6b2ab22b56831719f1101ecbb07735a5
hihassan1998 <hassanihussain1998@gmail.com>
2024-04-17 16:22:49 +0200

    backend complete with notes

--- p1/config/allowedOrigins.js
+++ p1/config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
+    
     'http://localhost:3000',
     'https://www.dandrepairshop.com',
+    // 'https://technotes-99q4.onrender.com/',
     'https://dandrepairshop.com'
 ]
 
--- p1/config/corsOptions.js
+++ p1/config/corsOptions.js
@@ -3,7 +3,7 @@ const allowedOrigins = require('./allowedOrigins')
 const corsOptions = {
     origin: (origin, callback) => {
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
+            callback(null, true) // can remove origins statement depending on if we need access from postman to the site
         } else {
             callback(new Error('Not allowed by CORS'))
         }
